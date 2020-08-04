4eac9b8fbb6584b0fec1d566275230b186efb325
Joyce Querubino <joycequerubino5@gmail.com>
2020-08-04 17:01:20 -0300

    Salvando arquivos

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing from './pages/Landing'; 
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <Landing/>
+  );
+}
+
+export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'; 
+
+function Routes(){
+    return(
+        <BrowserRouter>
+        
+        </BrowserRouter>
+    )
+}
