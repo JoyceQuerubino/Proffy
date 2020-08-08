b33621a3623fd671ce46374073427d6335c63346
Joyce Querubino <joycequerubino5@gmail.com>
2020-08-08 15:26:33 -0300

    Atualizando

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import {Link} from 'react-router-dom'; 
 
 import './styles.css';
@@ -8,10 +8,21 @@ import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg'; 
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'; 
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'; 
+import api from '../../services/api';
 
 
 
 function Landing(){
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+      api.get('connections').then(response => {
+        const {total} = response.data;
+
+        setTotalConnections(total);
+      })
+    }, []); 
+
     return(
         <div className="" id="page-landing">
         
@@ -41,7 +52,7 @@ function Landing(){
           </div>
 
         <span className="total-conections">
-                Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+                Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
 
 
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -5,14 +5,42 @@ import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 function TeacherList(){
     return (
        <div id="page-teacher-list" className="container">
           <PageHeader title="Estes são os professores disponíveis." >
               <form id="search-teachers">
-                  <Input name="subject" label="Matéria"/>
-                  <Input name="week_day" label="Dia da semana"/>
+                    <Select 
+                        name="subject" 
+                        label="Matéria" 
+                        options={[
+                            { value: 'Artes', label:'Artes'},
+                            { value: 'Biologia', label:'Biologia'},
+                            { value: 'Ciências', label:'Ciências'},
+                            { value: 'Educação Física', label:'Educação Física'},
+                            { value: 'Geografia', label:'Geografia'},
+                            { value: 'História', label:'História'},
+                            { value: 'Matemática', label:'Matemática'},
+                            { value: 'Portugês', label:'Portugês'},
+                            { value: 'Química', label:'Química'}
+                        ]}
+                    />
+
+                    <Select 
+                        name="week_day" 
+                        label="Dia da semana" 
+                        options={[
+                            { value: '0', label:'Domingo'},
+                            { value: '1', label:'Segunda-Feira'},
+                            { value: '2', label:'Terça-Feira'},
+                            { value: '3', label:'Quarta-Feira'},
+                            { value: '4', label:'Quinta-Feira'},
+                            { value: '5', label:'Sexta-Feira'},
+                            { value: '6', label:'Sábado'}
+                        ]}
+                    />
                   <Input type="time" name="time" label="Hora"/>
               </form>
           </PageHeader>
